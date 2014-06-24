7a3f265eef0250edeabfd13f0990a33657511175
Philip Basford <pjb@ecs.soton.ac.uk>
2014-06-24 14:03:36 +0100

    working stub, need tidying

--- code/adx_stub/adx_stub.ino
+++ code/adx_stub/adx_stub.ino
@@ -1,55 +1,81 @@
 #include <Wire.h>
+// https://github.com/jenschr/Arduino-libraries/blob/master/ADXL345/examples/ADXL345_no_library/BareBones_ADXL345.pde
+#define DEVICE (0x53) // Device address as specified in data sheet
 
-#define ACCEL_ADDR 0x53
-void setup() {
-  // put your setup code here, to run once:
-  Wire.begin();
-  Serial.begin(9600);
-  Serial.println("Waiting");
-  delay(5000);
-  Wire.beginTransmission(ACCEL_ADDR);
-  Wire.write(0x31);
-  Wire.write(0x00); //2g mode
-  Wire.endTransmission();
-  Wire.beginTransmission(ACCEL_ADDR);
-  Wire.write(0x2D);
-  Wire.write(0x80); //enable readings
-  Wire.endTransmission();
-  Serial.println("Starting");
-  int x1,x2,y1,y2,z1,z2;
-  double x, y, z;
-  Wire.beginTransmission(ACCEL_ADDR);
-  Wire.write(0x32);
-  Wire.requestFrom(ACCEL_ADDR, 6);
-  x1 = Wire.read();
-  x2 = Wire.read();
-  y1 = Wire.read();
-  y2 = Wire.read();
-  z1 = Wire.read();
-  z2 = Wire.read();
-  Wire.endTransmission();
-  x = x2 * 255 + x1;
-  y = y2 * 255 + y1;
-  z = z2 * 255 + y1;
-  Serial.print("X:");
-  Serial.println(x);
-  Serial.print("Y:");
-  Serial.println(y);
-  Serial.print("Z:");
-  Serial.println(z);
-  Serial.println("Done");
- 
- 
- // Conversion from http://theccontinuum.com/2012/09/24/arduino-imu-pitch-roll-from-accelerometer/
+byte _buff[6];
+
+char POWER_CTL = 0x2D;	//Power Control Register
+char DATA_FORMAT = 0x31;
+char DATAX0 = 0x32;	//X-Axis Data 0
+char DATAX1 = 0x33;	//X-Axis Data 1
+char DATAY0 = 0x34;	//Y-Axis Data 0
+char DATAY1 = 0x35;	//Y-Axis Data 1
+char DATAZ0 = 0x36;	//Z-Axis Data 0
+char DATAZ1 = 0x37;	//Z-Axis Data 1
+
+void setup()
+{
+  Wire.begin(); // join i2c bus (address optional for master)
+  Serial.begin(57000); // start serial for output. Make sure you set your Serial Monitor to the same!
+  Serial.print("init");
+  
+  //Put the ADXL345 into +/- 2G range by writing the value 0x01 to the DATA_FORMAT register.
+  writeTo(DATA_FORMAT, 0x00);
+  //Put the ADXL345 into Measurement Mode by writing 0x08 to the POWER_CTL register.
+  writeTo(POWER_CTL, 0x08);
+}
+
+void loop()
+{
+  readAccel(); // read the x/y/z tilt
+  delay(500); // only read every 0,5 seconds
+}
+
+void readAccel() {
+  uint8_t howManyBytesToRead = 6;
+  readFrom( DATAX0, howManyBytesToRead, _buff); //read the acceleration data from the ADXL345
+
+  // each axis reading comes in 10 bit resolution, ie 2 bytes. Least Significat Byte first!!
+  // thus we are converting both bytes in to one int
+  double x = (((int)_buff[1]) << 8) | _buff[0];
+  double y = (((int)_buff[3]) << 8) | _buff[2];
+  double z = (((int)_buff[5]) << 8) | _buff[4];
+  Serial.print("x: ");
+  Serial.print( x );
+  Serial.print(" y: ");
+  Serial.print( y );
+  Serial.print(" z: ");
+  Serial.println( z );
+   // Conversion from http://theccontinuum.com/2012/09/24/arduino-imu-pitch-roll-from-accelerometer/
    double roll, pitch;
     roll  = (atan2(-y, z)*180.0)/M_PI;
     pitch = (atan2(x, sqrt(y*y + z*z))*180.0)/M_PI;
     Serial.print(pitch);
     Serial.print(":");
-    Serial.print(roll);
+    Serial.println(roll);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void writeTo(byte address, byte val) {
+  Wire.beginTransmission(DEVICE); // start transmission to device
+  Wire.write(address); // send register address
+  Wire.write(val); // send value to write
+  Wire.endTransmission(); // end transmission
+}
 
+// Reads num bytes starting from address register on device in to _buff array
+void readFrom(byte address, int num, byte _buff[]) {
+  Wire.beginTransmission(DEVICE); // start transmission to device
+  Wire.write(address); // sends address to read from
+  Wire.endTransmission(); // end transmission
+
+  Wire.beginTransmission(DEVICE); // start transmission to device
+  Wire.requestFrom(DEVICE, num); // request 6 bytes from device
+
+  int i = 0;
+  while(Wire.available()) // device may send less than requested (abnormal)
+  {
+    _buff[i] = Wire.read(); // receive a byte
+    i++;
+  }
+  Wire.endTransmission(); // end transmission
 }
