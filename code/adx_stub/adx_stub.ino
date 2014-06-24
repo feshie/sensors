ff02705d10bae3fd6f3b0941f54870cca29b1f7a
Philip Basford <pjb@ecs.soton.ac.uk>
2014-06-24 14:36:15 +0100

    a bit more modular - still more work to do

--- code/adx_stub/adx_stub.ino
+++ code/adx_stub/adx_stub.ino
@@ -2,7 +2,9 @@
 // https://github.com/jenschr/Arduino-libraries/blob/master/ADXL345/examples/ADXL345_no_library/BareBones_ADXL345.pde
 #define DEVICE (0x53) // Device address as specified in data sheet
 
-byte _buff[6];
+#define DEBUG 1
+
+
 
 char POWER_CTL = 0x2D;	//Power Control Register
 char DATA_FORMAT = 0x31;
@@ -16,60 +18,69 @@ char DATAZ1 = 0x37;	//Z-Axis Data 1
 void setup()
 {
   Wire.begin(); // join i2c bus (address optional for master)
-  Serial.begin(57000); // start serial for output. Make sure you set your Serial Monitor to the same!
-  Serial.print("init");
-  
-  //Put the ADXL345 into +/- 2G range by writing the value 0x01 to the DATA_FORMAT register.
-  writeTo(DATA_FORMAT, 0x00);
-  //Put the ADXL345 into Measurement Mode by writing 0x08 to the POWER_CTL register.
-  writeTo(POWER_CTL, 0x08);
+  Serial.begin(9600); // start serial for output. Make sure you set your Serial Monitor to the same!
+
 }
+
+void setup_accel_wire(byte addr){
+  //Put the ADXL345 into +/- 2G range by writing the value 0x01 to the DATA_FORMAT register.
+  writeTo(DEVICE, DATA_FORMAT, 0x00);
+  //Put the ADXL345 into Measurement Mode by writing 0x08 to the POWER_CTL register.
+  writeTo(DEVICE, POWER_CTL, 0x08);
+  if (DEBUG){
+    Serial.println("ADXL setup");
+  }
+}
+
 
 void loop()
 {
   readAccel(); // read the x/y/z tilt
-  delay(500); // only read every 0,5 seconds
+  delay(1000); // only read every 0,5 seconds
 }
 
 void readAccel() {
   uint8_t howManyBytesToRead = 6;
-  readFrom( DATAX0, howManyBytesToRead, _buff); //read the acceleration data from the ADXL345
+  byte _buff[6];
+  readFrom( DEVICE, DATAX0, howManyBytesToRead, _buff); //read the acceleration data from the ADXL345
 
   // each axis reading comes in 10 bit resolution, ie 2 bytes. Least Significat Byte first!!
   // thus we are converting both bytes in to one int
   double x = (((int)_buff[1]) << 8) | _buff[0];
   double y = (((int)_buff[3]) << 8) | _buff[2];
   double z = (((int)_buff[5]) << 8) | _buff[4];
-  Serial.print("x: ");
-  Serial.print( x );
-  Serial.print(" y: ");
-  Serial.print( y );
-  Serial.print(" z: ");
-  Serial.println( z );
-   // Conversion from http://theccontinuum.com/2012/09/24/arduino-imu-pitch-roll-from-accelerometer/
-   double roll, pitch;
-    roll  = (atan2(-y, z)*180.0)/M_PI;
-    pitch = (atan2(x, sqrt(y*y + z*z))*180.0)/M_PI;
-    Serial.print(pitch);
-    Serial.print(":");
-    Serial.println(roll);
+  if (DEBUG){
+    Serial.print("x: ");
+    Serial.print( x );
+    Serial.print(" y: ");
+    Serial.print( y );
+    Serial.print(" z: ");
+    Serial.println( z );
+  }
+  // Conversion from http://theccontinuum.com/2012/09/24/arduino-imu-pitch-roll-from-accelerometer/
+  double roll, pitch;
+  roll  = (atan2(-y, z)*180.0)/M_PI;
+  pitch = (atan2(x, sqrt(y*y + z*z))*180.0)/M_PI;
+  Serial.print(pitch);
+  Serial.print(":");
+  Serial.println(roll);
 }
 
-void writeTo(byte address, byte val) {
-  Wire.beginTransmission(DEVICE); // start transmission to device
+void writeTo(byte device, byte address, byte val) {
+  Wire.beginTransmission(device); // start transmission to device
   Wire.write(address); // send register address
   Wire.write(val); // send value to write
   Wire.endTransmission(); // end transmission
 }
 
 // Reads num bytes starting from address register on device in to _buff array
-void readFrom(byte address, int num, byte _buff[]) {
-  Wire.beginTransmission(DEVICE); // start transmission to device
+void readFrom(byte device, byte address, int num, byte _buff[]) {
+  Wire.beginTransmission(device); // start transmission to device
   Wire.write(address); // sends address to read from
   Wire.endTransmission(); // end transmission
 
-  Wire.beginTransmission(DEVICE); // start transmission to device
-  Wire.requestFrom(DEVICE, num); // request 6 bytes from device
+  Wire.beginTransmission(device); // start transmission to device
+  Wire.requestFrom((int)device, num); // request 6 bytes from device
 
   int i = 0;
   while(Wire.available()) // device may send less than requested (abnormal)
