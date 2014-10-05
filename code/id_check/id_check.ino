37cbc9fecc357fed3cf56dc179800ba8c39fb4dd
Philip Basford <pjb@ecs.soton.ac.uk>
2014-10-05 13:55:30 +0100

    Now prints out in hex

--- code/id_check/id_check.ino
+++ code/id_check/id_check.ino
@@ -10,9 +10,9 @@ void setup() {
   digitalWrite(IO_PIN, HIGH);
   Serial.println("This program will print the ID from the AVR");
   Serial.print("The address is:");
-  Serial.println(EEPROM.read(0));
+  Serial.println(EEPROM.read(0), HEX);
   Serial.print("The mode is:");
-  Serial.println(EEPROM.read(1));
+  Serial.println(EEPROM.read(1), HEX);
   Serial.flush();
   digitalWrite(IO_PIN, LOW);
 }
